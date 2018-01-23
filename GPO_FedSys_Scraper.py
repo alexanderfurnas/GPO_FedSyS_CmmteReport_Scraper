cdac0d6b6de6916edf9931b0ee32cce10d7bf01b
alexanderfurnas <zfurnas@gmail.com>
2018-01-23 12:47:16 -0500

    updated gitignore, added scraper draft

---
+++ GPO_FedSys_Scraper.py
@@ -0,0 +1,78 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+import  os
+
+
+fedsys_search_url_pt1 = "https://www.gpo.gov/fdsys/search/search.action?sr="
+fedsys_search_url_pt2 = "&originalSearch=collection%3aCRPT&st=collection%3aCRPT&ps=100&na=_congressnum&se=_"
+fedsys_search_url_pt3 = "true&sb=re&timeFrame=&dateBrowse=&govAuthBrowse=&collection=&historical=true"
+
+def write_data(data, header, writepath):
+    ordered_fieldnames = header
+    if os.path.exists(writepath):
+        with open(writepath, "a") as dataset: 
+            writer = csv.writer(dataset, dialect = "excel")
+            writer.writerow(data)
+    else:
+        with open(writepath, "w") as dataset:
+            csv.DictWriter(dataset, dialect = "excel", fieldnames = ordered_fieldnames).writeheader()
+            writer = csv.writer(dataset, dialect = "excel")
+            writer.writerow(data)
+
+downloaded_files = []
+if os.path.exists('Downloaded_file_links.csv'): 
+	with open('Downloaded_file_links.csv', 'rb') as csvfile:
+		downloaded_files_reader = csv.reader(csvfile, dialect='excel')
+		next(downloaded_files_reader)
+		for row in downloaded_files_reader:
+			downloaded_files.append(row[0])
+else:
+	pass
+
+for congress in (range(104, 115)):
+	more_info_links = []
+	for page in range(1,17):
+	    r  = requests.get(fedsys_search_url_pt1 + str(page) +fedsys_search_url_pt2 + str(congress) + fedsys_search_url_pt3)
+	    soup = BeautifulSoup(r.text)
+	    search_results = soup.find("div", {"id":"search-results"})
+		items = search_results.find_all("table", {"class": "search-results-item"})
+		if len(items) > 0:
+		    link_lists = [i.find_all(href=True) for i in items]
+		    for links in link_lists:
+		        links = [ l.get("href") for l in links]
+		        more_info = "https://www.gpo.gov" + links[-1].replace("&amp;", "&")
+		        more_info_links.append(more_info)
+		    page_mssg = "Links collected from page %s" %page
+		    print(page_mssg)
+		else:
+			break
+
+	link_gather_mssg = "Link Collection Completed for %sth Congress" %s congress
+	print(link_gather_mssg)
+
+	print("Previous downloads loaded")
+
+	for link in more_info_links:
+		if link not in downloaded_files:
+			try:
+			    r  = requests.get(link)
+			    soup = BeautifulSoup(r.text)
+			    download_link = soup.find("table", {"class":"page-details-budget-metadata-table"}).find_all(href=True)[0].get("href")
+			    details = soup.find("div", {"id":"page-details-right-mask"})
+			    metadata = details.find("table", {"id":"page-details-metadata-table"})
+			    file_name =  download_link.split("/")[-1]
+			    report = requests.get(download_link)
+			    with open("Reports/" str(congress) +"/"+ file_name, 'wb') as outfile:
+			        outfile.write(report.content)
+			        
+			    with open("Meta_data/" +file_name + "metadata_table.csv", "w") as f:
+			        wr = csv.writer(f)
+			        wr.writerows([[td.text.encode("utf-8") for td in row.find_all("td")] for row in metadata.select("tr + tr")])    
+
+				write_data([link, congress], ["URL", "Congress"], "/Users/alexanderfurnas/Projects/Dissertation/Downloaded_file_links.csv") 
+			except:
+				write_data([link, congress], ["URL","Congress"], "/Users/alexanderfurnas/Projects/Dissertation/Not_downloaded_file_links.csv") 
+
+		else:
+			pass
